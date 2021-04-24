373d020e578b3429c266dc8343696d8fd45962ca
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-04-25 00:26:21 +0530

    Add materialize and icons.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
   return <div className='App'>My App</div>;
 };
 
